87378f111902016169d7aab58b76b3fb51fb28e8
Matheus Basilio <83681313+mathpravcs@users.noreply.github.com>
2021-12-08 23:31:09 -0300

    Add files via upload

---
+++ Porcentagem_Salario/src/application/program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Employee emp = new Employee();
+
+		System.out.print("Name: ");
+		emp.name = sc.nextLine();
+		System.out.print("Gross Salary: ");
+        emp.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        emp.tax = sc.nextDouble();
+        
+        System.out.println();
+        
+        System.out.println("Employee: " + emp);
+        System.out.println();
+        System.out.println("Which percentage to increase salary ? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+        
+        System.out.println();
+        System.out.println("Update data: " + emp);
+        
+        
+        sc.close();
+	
+	
+	
+	
+	}
+
+}
